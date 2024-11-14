42b9ff8bd78485d73d663769b01bdc67c8af867e
Prabin Pudasainee <aarp.been@gmail.com>
2024-11-14 12:22:40 +0545

    metadata changed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,8 +6,8 @@ import Testimonials from "./components/testimonials/testimonials";
 import CallToAction from "./components/callToAction/CallToAction";
 
 export const metadata: Metadata = {
-  title: "Learning Management System",
-  description: "A comprehensive platform for online learning, offering courses, resources, and community support to enhance your educational experience.",
+  title: "BS Global Business and Finance Research Pvt Ltd",
+  description: "BS Global Business and Finance Research Pvt. Ltd. delivers expert financial insights, consulting services, and research solutions to drive business growth and innovation.",
 };
 
 export default function Home() {
